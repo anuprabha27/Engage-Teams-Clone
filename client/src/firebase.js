d957df2b3b15820d5fd8e2b382e69287289efbbc
Anuprabha Bhardwaj <anuprabha.bhardwaj2000@gmail.com>
2021-07-10 15:59:18 +0530

    First commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,10 +2,5 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export const auth =  firebase.initializeApp({
-    apiKey: "AIzaSyAmO4hQCki5aUZIpZJ2gLqPhhrlozIWO0U",
-  authDomain: "teams-clone-30c29.firebaseapp.com",
-  projectId: "teams-clone-30c29",
-  storageBucket: "teams-clone-30c29.appspot.com",
-  messagingSenderId: "952931537479",
-  appId: "1:952931537479:web:078d58e86c813e72c35762"
+    {FIREBASE_CREDENTIALS}
   }).auth();
