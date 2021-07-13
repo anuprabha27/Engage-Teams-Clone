619bd515302f61b78a21aa67246c0fb51d9f763d
anuprabha27 <60571246+anuprabha27@users.noreply.github.com>
2021-07-13 16:35:47 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,1 +1,10 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 //add firebase config 
+/*
+  How to export?
+  export const auth = firebase.initializeApp({
+    //credentials
+  }).auth();
+*/
